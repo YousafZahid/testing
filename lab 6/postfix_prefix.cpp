ef0753ccd0147a3eafe360d18dde2e38058adeb9
YousafZahid <110010546+YousafZahid@users.noreply.github.com>
2023-03-12 20:37:12 +0500

    changed last commit

--- lab 6/postfix_prefix.cpp
+++ lab 6/postfix_prefix.cpp
@@ -11,11 +11,7 @@ int main()
 {
 	cout << "learning github";
 	cout << "modifying in github";
-	cout << "modifying in github";
-	cout << "modifying in github";
-	cout << "modifying in github";
-	cout << "modifying in github";
-	cout << "modifying in github";
+	
 
 	return 0;
 }
